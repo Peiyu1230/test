a3ce4bb8c5316b1fcf28b1faa38b098164578587
Peiyu1230 <pyy9512@gmail.com>
2021-01-18 20:16:26 +0800

    Add files via upload

---
+++ tixian.py
@@ -0,0 +1,8 @@
+import time,os
+
+import requests
+
+header={"Accept": "*/*","Accept-Encoding": "gzip, deflate, br","Accept-Language": "zh-Hans-US;q=1, zh-Hant-US;q=0.9, en-US;q=0.8, zh-Hant-TW;q=0.7, ja-JP;q=0.6","Connection": "keep-alive","Content-Length": "876","Content-Type": "application/x-www-form-urlencoded","Host": "ios.baertt.com","User-Agent": "KDApp/1.8.0 (iPhone; iOS 14.2; Scale/2.00)",}
+data=os.environ["rdata"]
+print(data)
+
