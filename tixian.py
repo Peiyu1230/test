38731eb4aa6a5911e4d15e3928832a7dd481741c
Peiyu1230 <pyy9512@gmail.com>
2021-01-18 20:45:29 +0800

    Update tixian.py

--- tixian.py
+++ tixian.py
@@ -1,10 +1,2 @@
-import time,os
-
-import requests
-
-header={"Accept": "*/*","Accept-Encoding": "gzip, deflate, br","Accept-Language": "zh-Hans-US;q=1, zh-Hant-US;q=0.9, en-US;q=0.8, zh-Hant-TW;q=0.7, ja-JP;q=0.6","Connection": "keep-alive","Content-Length": "876","Content-Type": "application/x-www-form-urlencoded","Host": "ios.baertt.com","User-Agent": "KDApp/1.8.0 (iPhone; iOS 14.2; Scale/2.00)",}
-if RDATA in os.environ:
-    data=RDATA
-    print(data)
-else:
-    print("没有。。。")
+def main():
+    data=input()
